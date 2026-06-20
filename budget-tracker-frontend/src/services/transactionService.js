1e5aef1b5ef5909439cbc78bc53e773f1570ffb8
Nishitha <nishithanishitha3434@gmail.com>
2026-06-20 14:40:34 +0530

    updated

--- budget-tracker-frontend/src/services/transactionService.js
+++ budget-tracker-frontend/src/services/transactionService.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/transactions";
+const API_URL =
+  "https://azentrix-fullstack-task1-backend.onrender.com/api/transactions";
 
 export const getTransactions = () =>
-    axios.get(API_URL);
+  axios.get(API_URL);
 
 export const addTransaction = (transaction) =>
-    axios.post(API_URL, transaction);
+  axios.post(API_URL, transaction);
 
 export const updateTransaction = (id, transaction) =>
-    axios.put(`${API_URL}/${id}`, transaction);
+  axios.put(`${API_URL}/${id}`, transaction);
 
 export const deleteTransaction = (id) =>
-    axios.delete(`${API_URL}/${id}`);
+  axios.delete(`${API_URL}/${id}`);
 
 export const getSummary = () =>
-    axios.get(`${API_URL}/summary`);
+  axios.get(`${API_URL}/summary`);
